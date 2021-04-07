2031b96609756317a51e9abbd09ef702f7450a02
BALAJI SARVEPALLI <sarvepallibalu@gmail.com>
2021-04-07 15:25:36 -0500

    Implemented post requests for pens

--- controllers/pen.js
+++ controllers/pen.js
@@ -14,9 +14,24 @@ exports.pen_detail = function(req, res) {
 res.send('NOT IMPLEMENTED: Pen detail: ' + req.params.id);
 };
 // Handle Pen create on POST.
-exports.pen_create_post = function(req, res) {
-res.send('NOT IMPLEMENTED: Pen create POST');
-};
+exports.pen_create_post = async function(req, res) {
+    console.log(req.body)
+    let document = new Pen();
+    // We are looking for a body, since POST does not have query parameters.
+    // Even though bodies can be in many different formats, we will be picky
+    // and require that it be a json object
+    // {"costumetype":"goat", "cost":12, "size":"large"}
+    document.pentype = req.body.pentype;
+    document.price = req.body.price;
+    document.color = req.body.color;
+    try{
+    let result = await document.save();
+    res.send(result);
+    }
+    catch(err){
+    res.error(500,`{"error": ${err}}`);
+    }
+    };
 // Handle Pen delete form on DELETE.
 exports.pen_delete = function(req, res) {
 res.send('NOT IMPLEMENTED: Pen delete DELETE ' + req.params.id);
