8d15172a6ce24f6d122f3e7d20e3eb5a8f35b39a
BALAJI SARVEPALLI <sarvepallibalu@gmail.com>
2021-04-07 15:09:13 -0500

    implemented list of pens

--- controllers/pen.js
+++ controllers/pen.js
@@ -1,7 +1,13 @@
 var Pen = require('../models/pen');
 // List of all Pens
-exports.pen_list = function(req, res) {
-res.send('NOT IMPLEMENTED: Pen list');
+exports.pen_list = async function(req, res) {
+    try{
+        thePens = await Pen.find();
+        res.send(thePens);
+        }
+        catch(err){
+        res.error(500,`{"error": ${err}}`);
+        }
 };
 // for a specific Pen.
 exports.pen_detail = function(req, res) {
