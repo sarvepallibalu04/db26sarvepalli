ef884683b692c1e003d604f4caf11557897dd86f
BALAJI SARVEPALLI <sarvepallibalu@gmail.com>
2021-04-09 18:11:38 -0500

    added Resource for update

--- controllers/pen.js
+++ controllers/pen.js
@@ -9,10 +9,7 @@ exports.pen_list = async function(req, res) {
         res.error(500,`{"error": ${err}}`);
         }
 };
-// for a specific Pen.
-exports.pen_detail = function(req, res) {
-res.send('NOT IMPLEMENTED: Pen detail: ' + req.params.id);
-};
+
 // Handle Pen create on POST.
 exports.pen_create_post = async function(req, res) {
     console.log(req.body)
@@ -20,7 +17,7 @@ exports.pen_create_post = async function(req, res) {
     // We are looking for a body, since POST does not have query parameters.
     // Even though bodies can be in many different formats, we will be picky
     // and require that it be a json object
-    // {"costumetype":"goat", "cost":12, "size":"large"}
+    // {"pentype":"goat", "cost":12, "size":"large"}
     document.pentype = req.body.pentype;
     document.price = req.body.price;
     document.color = req.body.color;
@@ -37,9 +34,23 @@ exports.pen_delete = function(req, res) {
 res.send('NOT IMPLEMENTED: Pen delete DELETE ' + req.params.id);
 };
 // Handle Pen update form on PUT.
-exports.pen_update_put = function(req, res) {
-res.send('NOT IMPLEMENTED: Pen update PUT' + req.params.id);
+exports.pen_update_put = async function(req, res) {
+    console.log(`update on id ${req.params.id} with body ${JSON.stringify(req.body)}`)
+    try {
+        let toUpdate = await Pen.findById( req.params.id)
+        // Do updates of properties
+        if(req.body.pentype) toUpdate.pentype = req.body.pentype;
+        if(req.body.color) toUpdate.cost = req.body.color;
+        if(req.body.price) toUpdate.price = req.body.price;
+        let result = await toUpdate.save();
+        console.log("Sucess " + result)
+        res.send(result)
+    } catch (err) {
+        res.status(500)
+        res.send(`{"error": ${err}: Update for id ${req.params.id} failed`);
+    }
 };
+
 // VIEWS
 // Handle a show all view
 exports.pen_view_all_Page = async function(req, res) {
@@ -51,3 +62,14 @@ exports.pen_view_all_Page = async function(req, res) {
     res.error(500,`{"error": ${err}}`);
     }
     };
+    // for a specific pen.
+exports.pen_detail = async function(req, res) {
+    console.log("detail"  + req.params.id)
+    try {
+        result = await Pen.findById( req.params.id)
+        res.send(result)
+    } catch (error) {
+        res.status(500)
+        res.send(`{"error": document for id ${req.params.id} not found`);
+    }
+};
