242d7eddafff6fdc8bf22ace875e6f8463c0129d
BALAJI SARVEPALLI <sarvepallibalu@gmail.com>
2021-04-12 16:40:41 -0500

    added creating a new instance of your collection

--- routes/pens.js
+++ routes/pens.js
@@ -8,4 +8,8 @@ router.get('/', pen_controlers.pen_view_all_Page);
 /* GET detail pen page */
 router.get('/detail', pen_controlers.Pen_view_one_Page);
 
+/* GET create pen page */
+router.get('/create', pen_controlers.pen_create_Page);
+
+
 module.exports = router;
